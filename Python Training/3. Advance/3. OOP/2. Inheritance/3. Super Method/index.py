6716ef034fbb379004cec802da432433d802694d
Deepakpatel-github <1506deepakpatel@gmail.com>
2024-07-29 20:10:39 +0530

    update is super method file

--- Python Training/3. Advance/3. OOP/2. Inheritance/3. Super Method/index.py
+++ Python Training/3. Advance/3. OOP/2. Inheritance/3. Super Method/index.py
@@ -1,14 +1,20 @@
 class Animal:
+    var = 0
+
     def __init__(self, breed, size, color, speed):
         self.breed = breed
         self.size = size
         self.color = color
         self.speed = speed
+        Animal.var += 1
         print("Parent Constructor")
         print(
             f"Breed: {self.breed}, Size: {self.size}, Color: {self.color}, Speed: {self.speed}"
         )
 
+    @classmethod
+    def get_var(cls):
+        print(cls.var)
 
 # Super method is used to call parent class constructor to the child class.
 class Dog(Animal):
@@ -18,11 +24,25 @@ class Dog(Animal):
         self.animal_name = animal_name
         print(f"Name: {self.animal_name}")
 
-    def bark(self):
+    def object_info(self):
+        print(f" ~ Object Information ~")
+        print(f"Animal name: {self.animal_name}")
+        print(f"Breed name: {self.breed}")
+        print(f"Size: {self.size}")
+        print(f"Color: {self.color}")
+        print(f"Speed: {self.speed} Km/h")
+
+    @staticmethod
+    def bark():
         print("woff woff")
 
 
 buddy = Dog("Dog", "IDK", 40, "blue", 50)
+buddy1 = Dog("Dog1", "IDK1", 1, "blu1e", 1)
+buddy2 = Dog("Dog2", "IDK2", 2, "blue2", 2)
+buddy.object_info()
 buddy.bark()
-
-
+buddy.get_var()
+buddy1.get_var()
+buddy2.get_var()
+buddy.get_var()
